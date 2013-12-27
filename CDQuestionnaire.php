4e06bb6c662b8c54a95ab8dae31f5e67174d3faa
denn <denn@dnweb02.datajapan.ne.jp>
2013-12-27 10:59:53 +0900

    Create SQL

--- CDQuestionnaire.php
+++ CDQuestionnaire.php
@@ -5,7 +5,7 @@ class CDQuestionnaire {
 	 *
 	 * @var unknown
 	 */
-	var $version = 0.123;
+	var $version = 0.125;
 	/**
 	 * データベースのテーブルの識別
 	 *
@@ -90,69 +90,74 @@ class CDQuestionnaire {
 	 */
 	function enquetesSql() {
 		return <<<EOS
-               CREATE TABLE {$this->tableName['enquetes']} (
-				    id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-				    name varchar(128) NOT NULL COMMENT 'アンケート名',
-				    start_date timestamp COMMENT '開始日',
-				    end_date timestamp COMMENT '終了日',
-				     -- 1:アンケート,2:問題
-				    poll_or_question int(1) unsigned DEFAULT 1 NOT NULL COMMENT 'アンケートor問題',
-				    PRIMARY KEY  (id)
-			     ) CHARACTER SET '$this->char',
-			    COMMENT = 'アンケートテーブル';
+CREATE TABLE {$this->tableName['enquetes']} (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(128) NOT NULL COMMENT 'アンケート名',
+  `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '開始日',
+  `end_date` timestamp NOT NULL DEFAULT '2034-12-31 23:59:50' COMMENT '終了日',
+  `poll_or_question` int(1) unsigned NOT NULL DEFAULT '0' COMMENT 'アンケートor問題',
+  `delete_flag` int(1) unsigned NOT NULL DEFAULT '1' COMMENT '削除フラグ',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '作成日',
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修正日',
+  PRIMARY KEY (`id`),
+  INDEX (`name`, `start_date`, `end_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET='$this->char' COMMENT='アンケートテーブル';
 EOS;
 	}
 	function questionsSql() {
 		return <<<EOS
-               CREATE TABLE {$this->tableName['questions']} (
-					id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-					-- 親テーブルenqueteのid(FK)
-					enquete_id bigint NOT NULL COMMENT 'enquete_id',
-					sort_id int NOT NULL COMMENT '表示順',
-					question_text varchar(256) NOT NULL COMMENT '問題文',
-					-- 1:単一回答,2:複数回答可
-					multiple_answer int(1) unsigned DEFAULT 1 NOT NULL COMMENT '回答が複数可か',
-					PRIMARY KEY  (id)
-					) CHARACTER SET '$this->char',
-					 COMMENT = '質問テーブル';
+CREATE TABLE {$this->tableName['questions']} (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `enquete_id` bigint(20) NOT NULL COMMENT 'enquete_id',
+  `sort_id` int(11) NOT NULL COMMENT '表示順',
+  `question_text` varchar(256) NOT NULL COMMENT '問題文',
+  `multiple_answer` int(1) unsigned NOT NULL DEFAULT '1' COMMENT '回答が複数可か',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '作成日',
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修正日',
+  PRIMARY KEY (`id`),
+  INDEX (`enquete_id`, `sort_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET='$this->char' COMMENT='質問テーブル';    
 EOS;
 	}
 	function selectionsSql() {
 		return <<<EOS
-               CREATE TABLE {$this->tableName['selections']} (
-					id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-					question_id bigint NOT NULL COMMENT 'question_id',
-					sort_id int NOT NULL COMMENT '表示順',
-					selection_display varchar(64) NOT NULL COMMENT 'selection_display',
-					-- アンケートでは無意味、trueだったらそれを選ぶと「正解」
-					to_select_flag boolean DEFAULT false NOT NULL COMMENT '選ぶべき選択肢',
-					PRIMARY KEY  (id)
-					) CHARACTER SET '$this->char',
-					 COMMENT = '選択肢テーブル';
+CREATE TABLE {$this->tableName['selections']} (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `question_id` bigint(20) NOT NULL COMMENT 'question_id',
+  `sort_id` int(11) NOT NULL COMMENT '表示順',
+  `selection_display` varchar(64) NOT NULL COMMENT 'selection_display',
+  `to_select_flag` tinyint(1) NOT NULL DEFAULT '0' COMMENT '選ぶべき選択肢',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '作成日',
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修正日',
+  PRIMARY KEY (`id`),
+  INDEX (`question_id`, `sort_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET='$this->char' COMMENT='選択肢テーブル';
 EOS;
 	}
 	function answersSql() {
 		return <<<EOS
-               CREATE TABLE {$this->tableName['answers']} (
-					id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-					enquete_id bigint NOT NULL COMMENT 'enquete_id',
-					question_id bigint NOT NULL COMMENT 'question_id',
-					selection_id bigint NOT NULL COMMENT 'selection_id',
-					identifier varchar(512) COMMENT '識別子',
-					PRIMARY KEY  (id)
-					) CHARACTER SET '$this->char',
-			       COMMENT = '回答結果テーブル';
+CREATE TABLE {$this->tableName['answers']} (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `enquete_id` bigint(20) NOT NULL COMMENT 'enquete_id',
+  `question_id` bigint(20) NOT NULL COMMENT 'question_id',
+  `selection_id` bigint(20) NOT NULL COMMENT 'selection_id',
+  `identifier` varchar(512) DEFAULT NULL COMMENT '識別子',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '作成日',
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修正日',
+  PRIMARY KEY (`id`),
+  INDEX (`enquete_id`, `question_id`, `selection_id`, `identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET='$this->char' COMMENT='回答結果テーブル';
 EOS;
 	}
 	function identifiersSql() {
 		return <<<EOS
-               CREATE TABLE {$this->tableName['identifiers']} (
-					id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-					enquete_id bigint NOT NULL COMMENT 'enquete_id',
-					identifier varchar(512) NOT NULL COMMENT 'identifier',
-					PRIMARY KEY  (id)
-					) CHARACTER SET '$this->char',
-				     COMMENT = '識別子テーブル';
+CREATE TABLE {$this->tableName['identifiers']} (
+  id bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  enquete_id bigint NOT NULL COMMENT 'enquete_id',
+  identifier varchar(512) NOT NULL COMMENT 'identifier',
+  PRIMARY KEY  (id)
+  ) CHARACTER SET '$this->char',
+  COMMENT = '識別子テーブル';
 EOS;
 	}
 }
